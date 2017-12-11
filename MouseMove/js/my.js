2eaa447eeece7d17db861107373fdb5003c12542
Your Name <VasyaBilak.vb@gmail.com>
2017-12-12 00:19:03 +0200

    first commit

---
+++ MouseMove/js/my.js
@@ -0,0 +1,24 @@
+'use strict'
+//function move(div){
+//    document.onmousedown=function(){return false}
+//    div.style.cursor='move';
+//    document.onmousemove=function(e){
+//        x=e.pageX;
+//        y=e.pageY;
+//        left=div.offsetLeft;
+//        top=div.offsetTop;
+//        left=x-left;
+//        top=y-top;
+//        document.onmousemove=function(e){
+//            x=e.pageX;
+//            y=e.pageY;
+//            div.style.top=y-top+'px';
+//            div.style.left=x-left+'px';
+//        }
+//    }
+//    document.onmouseup=function(){
+//        div.style.cursor='auto';
+//    document.onmousedown=function(){}
+//    document.onmousemove=function(){}
+//    }
+//}
